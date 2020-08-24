4acb3aae2a2a2755b951eeb4c700df0439caeaea
PiotrWolinski <55030310+PiotrWolinski@users.noreply.github.com>
2020-08-24 17:21:34 +0200

    Done screen in which game will be running

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
@@ -8,4 +8,20 @@ RESOLUTION = (720, 480)
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode()
+screen = pygame.display.set_mode(RESOLUTION)
+
+pygame.display.set_caption('Pong!')
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            sys.exit(0)
+
+    pressed = pygame.key.get_pressed()
+
+    if pressed[pygame.K_ESCAPE]:
+        running = False
+        sys.exit(0)
